62bc8b1eda1a7f1cc2a43d10b1a5a99542dcb87b
Maciej Procyk <macieekprocyk@gmail.com>
2025-06-21 10:56:41 +0200

    add custom UIViewController for iOS to enable no screen auto-locking when in app

--- composeApp/src/iosMain/kotlin/in/procyk/bring/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/in/procyk/bring/MainViewController.kt
@@ -1,5 +1,51 @@
 package `in`.procyk.bring
 
 import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.*
 
-fun MainViewController() = ComposeUIViewController { BringApp() }
+fun MainViewController(): UIViewController = object : UIViewController(nibName = null, bundle = null) {
+
+    private lateinit var composeViewController: UIViewController
+
+    override fun viewDidLoad() {
+        super.viewDidLoad()
+
+        composeViewController = ComposeUIViewController {
+            BringApp()
+        }
+
+        // Add as child view controller
+        addChildViewController(composeViewController)
+        view.addSubview(composeViewController.view)
+        composeViewController.didMoveToParentViewController(this)
+
+        // Configure constraints to fill the parent view
+        composeViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activateConstraints(
+            listOf(
+                composeViewController.view.topAnchor.constraintEqualToAnchor(view.topAnchor),
+                composeViewController.view.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor),
+                composeViewController.view.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+                composeViewController.view.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor)
+            )
+        )
+
+        // Disable idle timer when view loads
+        UIApplication.sharedApplication.setIdleTimerDisabled(true)
+    }
+
+    override fun viewWillDisappear(animated: Boolean) {
+        super.viewWillDisappear(animated)
+
+        // Re-enable idle timer when view disappears
+        UIApplication.sharedApplication.setIdleTimerDisabled(false)
+    }
+
+    override fun viewDidDisappear(animated: Boolean) {
+        super.viewDidDisappear(animated)
+        // Clean up child view controller
+        composeViewController.willMoveToParentViewController(null)
+        composeViewController.view.removeFromSuperview()
+        composeViewController.removeFromParentViewController()
+    }
+}
