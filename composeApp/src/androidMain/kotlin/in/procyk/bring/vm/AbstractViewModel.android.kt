6b0e919a3e5d359186479a53c6032b6cf2324e4a
Maciej Procyk <macieekprocyk@gmail.com>
2025-11-06 23:19:54 +0100

    cleanup `onShareList`

--- composeApp/src/androidMain/kotlin/in/procyk/bring/vm/AbstractViewModel.android.kt
+++ composeApp/src/androidMain/kotlin/in/procyk/bring/vm/AbstractViewModel.android.kt
@@ -1,24 +1,26 @@
 package `in`.procyk.bring.vm
 
-import android.content.ClipData
+import android.content.ClipData.newPlainText
 import android.content.Intent
 import android.content.Intent.ACTION_SEND
 import android.content.Intent.createChooser
 import androidx.compose.ui.platform.ClipEntry
 import bring.composeapp.generated.resources.Res
 import bring.composeapp.generated.resources.copied_list_id_to_clipboard
+import `in`.procyk.bring.vm.AbstractViewModel.Context
 
 actual fun updateListLocationPresentation(listId: String?) {}
 
-internal actual suspend fun onShareList(listId: String, context: AbstractViewModel.Context) {
+internal actual suspend fun onShareList(listId: String, context: Context) {
     val shareIntent = Intent(ACTION_SEND).apply {
         type = "text/plain"
         putExtra(Intent.EXTRA_TEXT, listId)
     }
-    context.platformContext.context.startActivity(createChooser(shareIntent, null).apply {
+    val intent = createChooser(shareIntent, null).apply {
         addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    })
+    }
+    context.platformContext.context.startActivity(intent)
 
-    context.clipboard.setClipEntry(ClipEntry(ClipData.newPlainText(null, listId)))
+    context.clipboard.setClipEntry(ClipEntry(newPlainText(null, listId)))
     context.showSnackbar(Res.string.copied_list_id_to_clipboard)
 }
--- composeApp/src/wasmJsMain/kotlin/in/procyk/bring/vm/AbstractViewModel.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/in/procyk/bring/vm/AbstractViewModel.wasmJs.kt
@@ -2,6 +2,7 @@ package `in`.procyk.bring.vm
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.ClipEntry.Companion.withPlainText
 import bring.composeapp.generated.resources.Res
 import bring.composeapp.generated.resources.copied_list_url_to_clipboard
 import `in`.procyk.bring.vm.AbstractViewModel.Context
@@ -9,6 +10,7 @@ import kotlinx.browser.window
 
 @OptIn(ExperimentalComposeUiApi::class)
 internal actual suspend fun onShareList(listId: String, context: Context) {
-    context.clipboard.setClipEntry(ClipEntry.withPlainText("${window.location.origin}/#${listId}"))
+    val clipEntry = withPlainText("${window.location.origin}/#${listId}")
+    context.clipboard.setClipEntry(clipEntry)
     context.showSnackbar(Res.string.copied_list_url_to_clipboard)
 }
