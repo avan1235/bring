e6f2a46e46cfdee0e59a98f1f42f908742860c72
Maciej Procyk <macieekprocyk@gmail.com>
2025-11-07 20:10:00 +0100

    try fixing web app theme color

--- composeApp/src/webMain/kotlin/in/procyk/bring/main.kt
+++ composeApp/src/webMain/kotlin/in/procyk/bring/main.kt
@@ -2,17 +2,26 @@
 
 package `in`.procyk.bring
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalFontFamilyResolver
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.window.ComposeViewport
 import com.materialkolor.ktx.toHex
 import `in`.procyk.bring.ui.BringAppTheme
+import `in`.procyk.bring.ui.Theme
+import `in`.procyk.bring.ui.Theme.*
 import `in`.procyk.bring.vm.PlatformContext
 import kotlinx.browser.document
 import kotlinx.browser.window
+import kotlinx.coroutines.flow.mapNotNull
+import org.w3c.dom.Element
+import org.w3c.dom.HTMLHeadElement
+import org.w3c.dom.HTMLMetaElement
 import org.w3c.dom.asList
 
 fun main() {
@@ -26,13 +35,17 @@ fun main() {
         when {
             fontsLoaded -> {
                 BringAppTheme(platformContext) { context ->
-                    val backgroundColor = BringAppTheme.colors.background
-                    LaunchedEffect(backgroundColor) {
-                        head.children.asList().single { it.getAttribute("name") == "theme-color" }.remove()
-                        document.createElement("meta").apply {
-                            setAttribute("name", "theme-color")
-                            setAttribute("content", backgroundColor.toHex())
-                        }.let(head::appendChild)
+                    val themeColorDarkMode by context.store.updates
+                        .mapNotNull { it?.run { themeColor to darkMode } }
+                        .collectAsState(Color.White.toArgb() to System)
+                    val isSystemInDarkTheme = isSystemInDarkTheme()
+                    LaunchedEffect(themeColorDarkMode) {
+                        val color = Color(themeColorDarkMode.first)
+                        sequenceOf(
+                            head.replaceThemeColor(color),
+                            head.replaceColorScheme(themeColorDarkMode.second, isSystemInDarkTheme),
+                            head.replaceLastStyle(color),
+                        ).forEach(head::appendChild)
                     }
                     BringAppInternal(
                         context = context,
@@ -53,6 +66,36 @@ fun main() {
             fontFamilyResolver.preload(fontFamily)
             fontsLoaded = true
         }
+    }
+}
+
+private fun HTMLHeadElement.replaceThemeColor(color: Color): Element {
+    children.asList().single { it is HTMLMetaElement && it.getAttribute("name") == "theme-color" }.remove()
+    return document.createElement("meta").apply {
+        setAttribute("name", "theme-color")
+        setAttribute("content", color.toHex())
+    }
+}
+
+private fun HTMLHeadElement.replaceColorScheme(theme: Theme, isSystemInDarkTheme: Boolean): Element {
+    children.asList().single { it is HTMLMetaElement && it.getAttribute("name") == "color-scheme" }.remove()
+    return document.createElement("meta").apply {
+        setAttribute("name", "color-scheme")
+        setAttribute(
+            "content",
+            when (theme) {
+                System -> if (isSystemInDarkTheme) "only dark" else "only light"
+                Dark -> "only dark"
+                Light -> "only light"
+            }
+        )
+    }
+}
+
+private fun HTMLHeadElement.replaceLastStyle(color: Color): Element {
+    children.asList().last { it.localName == "style" }.remove()
+    return document.createElement("style").apply {
+        innerHTML = "html, body { background: ${color.toHex()} !important; }"
     }
 }
 
