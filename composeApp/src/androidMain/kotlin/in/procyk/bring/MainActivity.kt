be75ff81751422144a8ff225591c882f9ed63a02
Maciej Procyk <macieekprocyk@gmail.com>
2026-02-18 21:33:58 +0100

    init filekit on android

--- composeApp/src/androidMain/kotlin/in/procyk/bring/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/in/procyk/bring/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import `in`.procyk.bring.vm.PlatformContext
+import io.github.vinceglb.filekit.FileKit
+import io.github.vinceglb.filekit.dialogs.init
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        FileKit.init(this)
         val platformContext = PlatformContext(this)
         setContent {
             BringApp(platformContext)
