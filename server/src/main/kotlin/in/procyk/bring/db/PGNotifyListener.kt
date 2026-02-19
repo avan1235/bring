8be57a47145742ff0e3aac2666094086fd96e30d
Maciej Procyk <macieekprocyk@gmail.com>
2026-02-19 16:59:03 +0100

    properly close the connection

--- server/src/main/kotlin/in/procyk/bring/db/PGNotifyListener.kt
+++ server/src/main/kotlin/in/procyk/bring/db/PGNotifyListener.kt
@@ -31,6 +31,7 @@ abstract class PGNotifyListener(
     override fun close() {
         pgConnection.createStatement().use { it.executeUpdate("UNLISTEN $channelName") }
         pgConnection.removeNotificationListener(uniqueName)
+        pgConnection.close()
     }
 
     protected abstract fun notification(payload: String)
