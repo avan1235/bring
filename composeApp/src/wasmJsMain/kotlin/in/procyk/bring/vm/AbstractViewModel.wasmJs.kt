9a2ae64c1c89337ac095becfd108224e87765989
Maciej Procyk <macieekprocyk@gmail.com>
2026-02-27 23:16:02 +0100

    fix messages for card

--- composeApp/src/wasmJsMain/kotlin/in/procyk/bring/vm/AbstractViewModel.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/in/procyk/bring/vm/AbstractViewModel.wasmJs.kt
@@ -3,6 +3,7 @@ package `in`.procyk.bring.vm
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.ClipEntry.Companion.withPlainText
 import bring.composeapp.generated.resources.Res
+import bring.composeapp.generated.resources.copied_card_id_to_clipboard
 import bring.composeapp.generated.resources.copied_list_url_to_clipboard
 import `in`.procyk.bring.vm.AbstractViewModel.Context
 import kotlinx.browser.window
@@ -17,5 +18,5 @@ internal actual suspend fun onShareList(listId: String, context: Context) {
 internal actual suspend fun onShareLoyaltyCard(cardId: String, context: Context) {
     val clipEntry = withPlainText(cardId)
     context.clipboard.setClipEntry(clipEntry)
-    context.showSnackbar(Res.string.copied_list_url_to_clipboard)
+    context.showSnackbar(Res.string.copied_card_id_to_clipboard)
 }
