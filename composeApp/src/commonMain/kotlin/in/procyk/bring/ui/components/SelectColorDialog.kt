c8bc973700368b73d31a05ece3e752561899cf08
Maciej Procyk <macieekprocyk@gmail.com>
2026-02-28 20:33:51 +0100

    improve color picker

--- composeApp/src/commonMain/kotlin/in/procyk/bring/ui/components/SelectColorDialog.kt
+++ composeApp/src/commonMain/kotlin/in/procyk/bring/ui/components/SelectColorDialog.kt
@@ -1,17 +1,25 @@
 package `in`.procyk.bring.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import bring.composeapp.generated.resources.*
 import com.github.skydoves.colorpicker.compose.BrightnessSlider
 import com.github.skydoves.colorpicker.compose.HsvColorPicker
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
+import com.materialkolor.ktx.toHex
+import `in`.procyk.bring.ui.BringAppTheme
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -22,8 +30,13 @@ internal fun SelectColorDialog(
     showBrightnessSlider: Boolean = true,
     onColorSaved: (Color) -> Unit,
 ) {
+    val selectedColor = if (selectedColor == Color.Unspecified) BringAppTheme.colors.primary else selectedColor
     AnimatedVisibility(previousColor.value != null) {
+        val density = LocalDensity.current
         val controller = rememberColorPickerController()
+        LaunchedEffect(controller) {
+            controller.wheelPaint = density.wheelPaint()
+        }
         fun onDismissRequest() {
             previousColor.value?.let(onColorSaved)
             previousColor.value = null
@@ -62,23 +75,51 @@ internal fun SelectColorDialog(
             text = {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
                 ) {
-                    HsvColorPicker(
-                        modifier = Modifier.fillMaxWidth().height(240.dp).padding(10.dp),
-                        controller = controller,
-                        initialColor = selectedColor,
-                        onColorChanged = { onColorSaved(it.color) }
-                    )
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        var currentColor by remember { mutableStateOf(selectedColor) }
+                        Column(
+                            modifier = Modifier.width(120.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .border(1.dp, BringAppTheme.colors.primary, RoundedCornerShape(8.dp))
+                                    .background(currentColor, RoundedCornerShape(8.dp))
+                                    .size(64.dp)
+                            )
+                            Text(text = currentColor.toHex())
+                        }
+                        HsvColorPicker(
+                            modifier = Modifier.size(240.dp).padding(10.dp),
+                            controller = controller,
+                            initialColor = selectedColor,
+                            onColorChanged = {
+                                currentColor = it.color
+                                onColorSaved(it.color)
+                            }
+                        )
+                    }
                     if (showBrightnessSlider) BrightnessSlider(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(10.dp)
-                            .height(32.dp),
+                        modifier = Modifier.size(width = 380.dp, height = 32.dp),
                         controller = controller,
                         initialColor = selectedColor,
+                        wheelPaint = density.wheelPaint(),
                     )
                 }
             },
         )
     }
+}
+
+private fun Density.wheelPaint(): Paint = Paint().apply {
+    color = Color.White
+    alpha = 1.0f
+    style = PaintingStyle.Stroke
+    strokeWidth = with(this@wheelPaint) { 2.dp.toPx() }
 }
