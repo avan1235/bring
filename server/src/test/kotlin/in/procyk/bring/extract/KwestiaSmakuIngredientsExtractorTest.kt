28524850c24c64ed58ed865f640241724a976b58
Maciej Procyk <macieekprocyk@gmail.com>
2026-06-03 22:10:18 +0200

    update test data

--- server/src/test/kotlin/in/procyk/bring/extract/KwestiaSmakuIngredientsExtractorTest.kt
+++ server/src/test/kotlin/in/procyk/bring/extract/KwestiaSmakuIngredientsExtractorTest.kt
@@ -3,10 +3,12 @@ package `in`.procyk.bring.extract
 internal class KwestiaSmakuIngredientsExtractorTest : IngredientsExtractorTest(
     KwestiaSmakuIngredientsExtractor,
     "https://www.kwestiasmaku.com/przepis/sernik-baskijski",
-    "1 kg serka kremowego (cream cheese)*",
-    "300 g cukru",
+    "150 g herbatników (np. petit beurre, holenderskie)",
+    "50 g masła",
+    "1 kg solonego serka kremowego (cream cheese)*",
+    "250 g cukru",
     "1 łyżka cukru wanilinowego lub 2 łyżeczki ekstraktu",
     "3 łyżki mąki pszennej",
     "5 jajek",
-    "200 g śmietanki 36% lub 30%"
+    "200 g śmietanki 30%"
 )
